9074e05c577ef91bb1f237716e778fda4a182e21
JongKook <94429120+JongKookE@users.noreply.github.com>
2022-12-07 14:39:46 +0900

    Add files via upload

--- DB/upload.sql
+++ DB/upload.sql
@@ -1,14 +1,79 @@
-create table upload(
-   num int not null auto_increment,
-   id char(15) not null,
-   name char(10) not null,
-   subject char(200) not null,
-   content text not null,        
-   regist_day char(20) not null,
-   hit int not null,
-   file_name char(40),
-   file_type char(40),
-   file_copied char(40),
-   primary key(num)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 22-12-07 06:38
+-- 서버 버전: 10.4.25-MariaDB
+-- PHP 버전: 8.1.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `fti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `upload`
+--
+
+CREATE TABLE `upload` (
+  `num` int(11) NOT NULL,
+  `id` char(15) NOT NULL,
+  `name` char(10) NOT NULL,
+  `subject` char(200) NOT NULL,
+  `content` text NOT NULL,
+  `regist_day` char(20) NOT NULL,
+  `hit` int(11) NOT NULL,
+  `file_name` char(40) DEFAULT NULL,
+  `file_type` char(40) DEFAULT NULL,
+  `file_copied` char(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 테이블의 덤프 데이터 `upload`
+--
+
+INSERT INTO `upload` (`num`, `id`, `name`, `subject`, `content`, `regist_day`, `hit`, `file_name`, `file_type`, `file_copied`) VALUES
+(5, 'hj', '곽희진', '교수님', '열심히 했습니다 예쁘게 봐주세요', '2022-12-06 (08:02)', 39, 'EGPdt1mVAAIYqEz.jpg', 'image/jpeg', '2022_12_06_08_02_42.jpg'),
+(10, 'hj', '곽희진', '박효신', '잘생겼다.', '2022-12-06 (08:58)', 3, 'avatars-000140731112-fa8o31-t240x240.jpg', 'image/jpeg', '2022_12_06_08_58_26.jpg'),
+(19, 'jongkookE', '박종국', '조유리', '조유리', '2022-12-06 (13:07)', 0, '308585_354322_1814.jpg', 'image/jpeg', '2022_12_06_13_07_28.jpg'),
+(21, 'jongkookE', '박종국', '분위기 있는...', '방에 모기가 있어', '2022-12-06 (13:08)', 0, 'pfo1f15bwb549s548u7h.jpg', 'image/jpeg', '2022_12_06_13_08_24.jpg'),
+(22, 'jongkookE', '박종국', '안녕~', '봐줘서 고마워요~', '2022-12-06 (13:09)', 0, 'bef59207f5155a4eddd632c9a833e80d7154249a', 'image/png', '2022_12_06_13_09_06.png'),
+(23, 'jongkookE', '박종국', '귀여운', '사진', '2022-12-06 (13:10)', 6, '제목을_입력해주세요._1.png', 'image/png', '2022_12_06_13_10_10._1'),
+(24, 'hj', '곽희진', '김채원', '김채원', '2022-12-07 (05:08)', 0, 'article.jpg', 'image/jpeg', '2022_12_07_05_08_40.jpg');
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `upload`
+--
+ALTER TABLE `upload`
+  ADD PRIMARY KEY (`num`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `upload`
+--
+ALTER TABLE `upload`
+  MODIFY `num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- DB/users.sql
+++ DB/users.sql
@@ -1,11 +1,75 @@
-create table users (
-    num int not null auto_increment,
-    id char(15) not null,
-    pass char(15) not null,
-    name char(10) not null,
-    email char(80),
-    regist_day char(20),
-    level int,
-    point int,
-    primary key(num)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 22-12-07 06:38
+-- 서버 버전: 10.4.25-MariaDB
+-- PHP 버전: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `fti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `users`
+--
+
+CREATE TABLE `users` (
+  `num` int(11) NOT NULL,
+  `id` char(15) NOT NULL,
+  `pass` char(15) NOT NULL,
+  `name` char(10) NOT NULL,
+  `email` char(80) DEFAULT NULL,
+  `regist_day` char(20) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `point` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 테이블의 덤프 데이터 `users`
+--
+
+INSERT INTO `users` (`num`, `id`, `pass`, `name`, `email`, `regist_day`, `level`, `point`) VALUES
+(1, 'hj', '1234', '곽희진', 'pix3000@naver.com', NULL, NULL, NULL),
+(2, 'hj', '', '곽희진', 'pix3000@naver.com', NULL, NULL, NULL),
+(3, 'hj', '', '', '@', NULL, NULL, NULL),
+(4, 'jongkookE', '1234', '박종국', 'whdrnrdl789@naver.com', NULL, NULL, NULL),
+(5, 'khj', '1234', '곽희진', '1123123@naver.com', NULL, NULL, NULL);
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`num`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
